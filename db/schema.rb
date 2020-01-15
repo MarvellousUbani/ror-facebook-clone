b3a179e137284862dcf02a2e4b05efb6d1e055e0
Ashah15 <ashaabdillahi53@gmail.com>
2020-01-15 19:05:34 +0300

    Modify manifest.js,_head.html.erb,webpacker.yml,shema.rb,development.log&&bootsnap-load-path-cache

--- db/schema.rb
+++ db/schema.rb
@@ -46,9 +46,6 @@ ActiveRecord::Schema.define(version: 2020_01_10_081257) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name"
-    t.string "location"
-    t.text "description"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
