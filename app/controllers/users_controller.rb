244b9451e57eb93828fd81b5698749859523fccd
stickler-ci <support@stickler-ci.com>
2020-03-09 15:35:55 +0000

    Fixing style errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     @confirmed_friends = Friendship.confirmed_requests(current_user.id)
     @accept_friends = Friendship.accept_requests(current_user.id)
     @not_friends = User.where.not(
-          id: (@confirmed_friends.map(&:id) + @pending_friends.map(&:id))).reject{ |user| user == current_user }
+      id: (@confirmed_friends.map(&:id) + @pending_friends.map(&:id))
+    ).reject { |user| user == current_user }
   end
 
   def show
