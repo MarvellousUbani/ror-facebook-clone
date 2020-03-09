a15f3a51b5a9242c9894bf909efeaf2474cd31e4
Ashah15 <ashaabdillahi53@gmail.com>
2020-03-09 18:35:02 +0300

    Modify users_controller.rb && index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
     @confirmed_friends = Friendship.confirmed_requests(current_user.id)
     @accept_friends = Friendship.accept_requests(current_user.id)
     @not_friends = User.where.not(
-      id: current_user.friendships.map(&:friend_id) + current_user.friendships.map(&:user_id)
-    )
+          id: (@confirmed_friends.map(&:id) + @pending_friends.map(&:id))).reject{ |user| user == current_user }
   end
 
   def show
