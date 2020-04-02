15cf67a82ad2b98a60f720f5d1773e66e33c94bf
Ashah15 <ashaabdillahi53@gmail.com>
2020-04-03 00:43:20 +0300

    Modify users_controller.rb && index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[show index]
 
-  before_action :user_friends, only: %i[show index]
-
   def index
     @pending_friends = Friendship.pending_requests(current_user.id)
+    @confirmed_friends = Friendship.confirmed_requests(current_user.id)
     @not_friends = User.where.not(
-      id: (@confirmed_friends.map(&:friend_id) + @pending_friends.map(&:friend_id))
-    )
+      id: @confirmed_friends.map(&:user_id))
   end
 
   def show
@@ -18,10 +16,4 @@ class UsersController < ApplicationController
     @posts = @user.posts
   end
 
-  private
-
-  def user_friends
-    @confirmed_friends = Friendship.confirmed_requests(current_user.id)
-    @accept_friends = Friendship.accept_requests(current_user.id)
-  end
 end
